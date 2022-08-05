f2df59caa13a9bf05c0f00a113a08b8019d93eb8
vagrant <vagrant@localhost.localdomain>
2022-08-05 05:30:54 +0000

    added apache server setup recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,4 @@
 # Recipe:: default
 #
 # Copyright (c) 2022 The Authors, All Rights Reserved.
+include_recipe 'apache::server'
--- recipes/server.rb
+++ recipes/server.rb
@@ -3,3 +3,15 @@
 # Recipe:: /server
 #
 # Copyright (c) 2022 The Authors, All Rights Reserved.
+
+package 'httpd' do 
+   action :install
+end
+
+file '/var/www/html/index.html' do
+     content '<h1> Hello, world!</h1>'
+end
+
+service 'httpd' do 
+   action [:start,:enable]
+end
